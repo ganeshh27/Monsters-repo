92f944776c57e59d08d764b25c255b55db12ef0c
ganeshh27 <ganeshh27@gmail.com>
2020-10-20 13:19:26 +0530

    Updates

---
+++ precache-manifest.dec51617930e99e71e8f1fa7e6e4ab30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ae77dac7b7a2815d2a1ac6b107e681d",
+    "url": "/Monsters-repo/index.html"
+  },
+  {
+    "revision": "8c45968934949681665d",
+    "url": "/Monsters-repo/static/css/main.7009515c.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/Monsters-repo/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Monsters-repo/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c45968934949681665d",
+    "url": "/Monsters-repo/static/js/main.3cf9eefb.chunk.js"
+  },
+  {
+    "revision": "844f61fd1a503370c4cf",
+    "url": "/Monsters-repo/static/js/runtime-main.559e66ce.js"
+  }
+]);
